c13662ee6bdea3f234c0ccc74ce55e8b112e535a
sanekmolodoy <sanekmolodoy@gmail.com>
2015-09-27 23:36:18 +0500

    Готовы раб. #1, 2, 3, 4, 5, обновлена лаб. #6

--- lab6/ex1/ex1/main.cpp
+++ lab6/ex1/ex1/main.cpp
@@ -19,6 +19,8 @@
 
 using namespace std;
 
+void getWords (char* par);
+
 int main()
 {
 	setlocale(LC_ALL,"Russian");
@@ -34,8 +36,40 @@ int main()
 	cout << "Вы ввели: " << paragraph << endl;
 	cout << "----------------------------------------------" << endl;
 
+	getWords(paragraph);
 
 	return 0;
 }
 
+void getWords (char par[])
+{
+	char firstWord[50];
+	char nextWord[50];
 
+	for (int i = 1; par[i] != '\0'; i++)
+	{
+		if (par[i] == ' ' && par[i-1] != ' ')
+		{
+			strncpy(firstWord, par, i);
+			firstWord[i] = '\0';
+		}
+	}
+
+	for (int i = 1; par[i] != '.'; i++)
+	{
+		int x;
+		if (par[i] == ' ' && par[i-1] != ' ')
+		{
+			strncpy(nextWord, par, i-x);
+			nextWord[i] = '\0';
+			x = i;
+		}
+
+		cout << "'" << nextWord << "'" << " cmps with " << "'" << firstWord << "'" << endl; 
+
+		if (strcmp(firstWord, nextWord) == 0)
+			cout << nextWord << endl;
+	}
+
+	//cout << firstWord;
+}
