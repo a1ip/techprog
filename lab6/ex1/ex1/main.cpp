4fb4e41a6c650045df4380379c6112c3fa6c2df2
sanekmolodoy <sanekmolodoy@gmail.com>
2015-10-05 02:25:28 +0500

    Готовы лабораторные #1-6

--- lab6/ex1/ex1/main.cpp
+++ lab6/ex1/ex1/main.cpp
@@ -19,57 +19,120 @@
 
 using namespace std;
 
-void getWords (char* par);
+#define BUF_SIZE 256
+
+int wordCount(char* sentence);
+
+char** wordList(char* sentence, int itemsNum);
+
+int wordEntry(char** list, char* word, int itemsNum);
 
 int main()
 {
 	setlocale(LC_ALL,"Russian");
 
-	char paragraph[256];
-
 	cout << "Лабораторная работа 6: Строки" << endl;
 	cout << "----------------------------------------------" << endl;
 
-	cout << "Введите строку: ";
-	gets(paragraph);
+	//Исходное предложение:
+	char* paragraph = "что-то на русском языке; скорее всего, это что-то даже невозможно будет прочитать; но я всё же постараюсь что-то сделать.";
 
 	cout << "Вы ввели: " << paragraph << endl;
 	cout << "----------------------------------------------" << endl;
 
-	getWords(paragraph);
+	//Подсчёт количества слов:
+	int words = wordCount(paragraph);
+
+	//Форирование списка слов:
+	char** list = wordList(paragraph, words);
+
+	//Число совпадений слов списка с первым словом из него:
+	int entries = wordEntry(list, list[0], words);
+
+	cout << "Число слов, совпадающих с первым словом строки: " << entries << endl;
 
 	return 0;
 }
 
-void getWords (char par[])
+/*
+Подсчитывает число слов в предложении
+*/
+int wordCount(char* sentence)
 {
-	char firstWord[50];
-	char nextWord[50];
-
-	for (int i = 1; par[i] != '\0'; i++)
-	{
-		if (par[i] == ' ' && par[i-1] != ' ')
+	int count = 0;	//счётчик слов
+	
+	//Цикл по всему предложению:
+	int i = 0;	//счётчик цикла
+	do 
+	{	
+		//Условие проверки конца слова:
+		if ((sentence[i+1] == ' ' && (sentence[i] != '.') && (sentence[i] != ' ')) || (sentence[i+1] == '.' && (sentence[i] != '.') && (sentence[i] != ' ')))
 		{
-			strncpy(firstWord, par, i);
-			firstWord[i] = '\0';
+			count++;
+		}
+		i++;
+
+	} while (sentence[i] != '.' && sentence[i+1] != '\0' );
+
+	return count;
+}
+
+/*
+Формирует список слов из одного преложения
+*/
+char** wordList(char* sentence, int itemsNum)
+{
+	char** list;	//список результатов
+
+	list = new char* [itemsNum];	//выделяем память под сам список
+	
+	//Цикл по всему предложению:
+	int i = 0;		//счётчик цикла
+	int count = 0;	//счётчик слов
+	char* buf = new char [BUF_SIZE];	//выделяем буфер для слова
+	do 
+	{		
+		//Если слово началось:
+		if (sentence[i] != ' ' && sentence[i] != '.')
+		{	
+			int j = i;	//фиксируем позицию начала слова
+			//Цикл по одному слову:
+			do 
+			{
+				buf[j-i] = sentence[j];	//заполняем буфер посимвольно
+				j++;					//смещаем позицию
+			} while (sentence[j] != ' ' && sentence[j] != '.');
+
+			buf[j - i] = 0;				//j-i - последняя позиция в слове
+			list[count] = new char [j - i + 1];	//выделяем память для слова в списке: 
+												//j-i+1 - длина слова с учётом терминального нуля
+			strcpy(list[count++], buf);	//копируем из буфера в список
+
+			i = j;	//"проматываем" счётчик на следующий последний символ слова
+		}
+		i++;
+	} while (sentence[i] != '.' && sentence[i+1] != '\0' );
+
+	//возврат указателя на список
+	return list;
+}
+
+/*
+Подсчитывает количество совпадений слова в списке
+*/
+int wordEntry(char** list, char* word, int itemsNum)
+{
+	int entryCount = 0;	//счётчик количества совпадений
+
+	//Цикл по всему списку:
+	for (int i = 0; i < itemsNum; i++)
+	{
+		//Если слова равны
+		if (!strcmp(list[i], word))
+		{
+			entryCount++;	//увеличить счётчик
 		}
 	}
 
-	for (int i = 1; par[i] != '.'; i++)
-	{
-		int x;
-		if (par[i] == ' ' && par[i-1] != ' ')
-		{
-			strncpy(nextWord, par, i-x);
-			nextWord[i] = '\0';
-			x = i;
-		}
-
-		cout << "'" << nextWord << "'" << " cmps with " << "'" << firstWord << "'" << endl; 
-
-		if (strcmp(firstWord, nextWord) == 0)
-			cout << nextWord << endl;
-	}
-
-	//cout << firstWord;
+	return entryCount;
 }
